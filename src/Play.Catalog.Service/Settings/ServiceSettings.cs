fd394c1762abf1c1b10d745554f00ef705fb51d0
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-08-09 17:26:06 +0100

    Created the ServiceSettings class.

---
+++ src/Play.Catalog.Service/Settings/ServiceSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Catalog.Service.Settings
+{
+    public class ServiceSettings
+    {
+        //We use the "init" feature to ensure that the specified Host and Port aren't changed after the project is initialized
+        public string ServiceName { get; init; }
+    }
+}
